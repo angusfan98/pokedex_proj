3a1d90928f7d6833c8f1e191a5a3bc00e96d19e4
Angus Fan <angusf@sfu.ca>
2022-05-13 04:48:56 -0700

    info card working onclick but requires 2 clicks

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
-import { useState } from 'react'
 import PokemonDisplay from './PokemonDisplay'
+import {arr} from './PokemonDisplay'
+import './display.css'
 
 
 function Pokedex() {
     const[pokemon_list, setAllPokemons] = useState([])
     const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
-
+    const [tmp,settmp] = useState([arr])
     const get_pokemon = async () => {
     const res = await fetch(loadMore)
     const data = await res.json()
@@ -24,26 +25,38 @@ function Pokedex() {
     createPokemonObject(data.results)
 
   }
+
+  function set_tmp(){
+    settmp([arr])
+  }
  
   function sort_pokemon(poke_arr){
     poke_arr.sort((a,b)=>(a.name > b.name)?1:-1)
   }
 
+useEffect(() => {
+  get_pokemon()
+  }, [])
 
  useEffect(() => {
-  get_pokemon()
- }, [])
+  console.log(tmp)
+ }, [tmp])
 
 
 const {isAuthenticated} = useAuth0();
-
   return (
     isAuthenticated && (
         <>
         <button onClick={()=>sort_pokemon(pokemon_list)}>sort</button>
+        <div>
+        <h1>{tmp[0][0]}</h1>
+        <h1>{tmp[0][1]}</h1>
+        <h1>{tmp[0][2]}</h1>
+        <h1>{tmp[0][3]}</h1>
+        </div>
         <div className="app-container">
         <div className="pokemon-container">
-          <div className="all-container">
+          <div className="all-container" onClick={()=>set_tmp()}>
             {pokemon_list.map( (pokemonStats, index) => 
               <PokemonDisplay
                 key={index}
@@ -52,7 +65,6 @@ const {isAuthenticated} = useAuth0();
                 name={pokemonStats.name}
                 type={pokemonStats.types[0].type.name}
               />)}
-            
           </div>
             <button onClick={() => get_pokemon()}>↓</button>
         </div>
--- src/components/PokemonDisplay.js
+++ src/components/PokemonDisplay.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import './display.css'
+import Axios from "axios";
 
-const PokemonDisplay = ({id, image, name, type}) => {
+export var arr = [];
+
+function get_data(id){
+   Axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then(
+       (response)=>{
+       arr = [response.data.forms[0].name,response.data.stats[1].base_stat,response.data.stats[2].base_stat,response.data.types[0].type.name]
+       });
+    return arr
+}
+
+const PokemonDisplay = ({ id, image, name, type}) => {
     const style = type + " thumb-container";
     return (
-        <div className={style}>
+        <div className={style} onClick={() => (get_data(id))}>
             <div className="number"><small>#{id}</small></div>
             <img src={image} alt={name} />
             <div className="detail-wrapper">
@@ -13,5 +24,6 @@ const PokemonDisplay = ({id, image, name, type}) => {
         </div>
     )
 }
+
 
 export default PokemonDisplay
